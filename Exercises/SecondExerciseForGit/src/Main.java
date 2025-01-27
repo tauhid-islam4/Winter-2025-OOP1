29a335f65acdfe1c9e958511d65b6d4bf3edd721
tauhid-islam4 <tauhid.islam4@gmail.com>
2025-01-27 18:41:57 -0500

    Commit for Centered left triangle exercise for git

--- Exercises/SecondExerciseForGit/src/Main.java
+++ Exercises/SecondExerciseForGit/src/Main.java
@@ -1,15 +1,63 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //demonstrateSwitchCase();
+        printCenteredTriangle();
+    }
+
+    private static void demonstrateSwitchCase(){
+        String[] fruits  = {"apple", "banana", "orange", "pear", "pineapple"};
+        //String choice1 = "banana";
+        //String choice2 = "mango";
+        Scanner input = new Scanner(System.in);
+        String choice;
+        choice = input.nextLine();
+        switch(choice){
+            case "apple":
+                System.out.println("Fruit is: Apple");
+                break;
+            case "banana":
+                System.out.println("Fruit is: Banana");
+                break;
+            case "orange":
+                System.out.println("Fruit is: Orange");
+                break;
+            case "pear":
+                System.out.println("Fruit is: Pear");
+                break;
+            case "pineapple":
+                System.out.println("Fruit is: Pineapple");
+                break;
+            default:
+                System.out.println("Not a valid fruit");
+                break;
         }
+    }
+
+    private static void printCenteredTriangle(){
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for the number of rows
+        System.out.print("Enter the number of rows for the triangle: ");
+        int rows = scanner.nextInt();
+
+        // Generate the centered triangle
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces for centering
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            // Print asterisks for the row
+            for (int j = 1; j <= (2 * i - i); j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+
+        scanner.close();
     }
 }
